a15c5b2e98df0120d8fd8d75bc28b20202daf25c
Davesuy <davesuy@gmail.com>
2020-03-31 12:24:24 +0800

    update code

--- public/class-wp-ontraninja-shortcodes.php
+++ public/class-wp-ontraninja-shortcodes.php
@@ -48,6 +48,11 @@ class Wp_Ontraninja_Shortcodes {
 		add_shortcode( 'wp_ontraninja_cot_field', array($this,'wp_ontraninja_cot_field_func') );
 		add_shortcode( 'wp_ontraninja_contact_field', array($this,'wp_ontraninja_contact_field_func') );
 
+		add_shortcode( 'wp_ontraninja_fieldeditor', array($this,'wp_ontraninja_fieldeditor_func') );
+
+
+		add_shortcode( 'wp_ontraninja_register_information', array($this,'wp_ontraninja_register_information_func') );
+
 	}
 
 	public function wp_ontraninja_cot_field_func($atts) {
@@ -262,6 +267,350 @@ class Wp_Ontraninja_Shortcodes {
 	    } 
 
 	}
+
+
+	public function wp_ontraninja_fieldeditor_func($atts) {
+
+		$atts = shortcode_atts( array(
+	        'object_id' => '',
+	        'field_alias' => '',
+	        'section' => ''
+	    ), $atts, 'wp_ontraninja_fieldeditor' );
+
+
+	    // Retrieve Fields
+
+    	$requestParamsFieldeditor = array(
+		    "objectID" => $atts['object_id'],
+		    "section" => $atts['section']
+		 
+		);
+
+		$response_field = $this->client->object()->retrieveFields($requestParamsFieldeditor);
+
+		$response_field_decode = json_decode($response_field);
+
+	
+
+		$fields = $response_field_decode->data->fields[0];
+
+		
+
+		foreach($fields as $field) {
+
+
+			
+			if($field->alias == $atts['field_alias']) {
+
+				$field_output = $field->field;
+
+			}
+
+
+		}
+
+		//return'<pre>'.print_r($field_output, true).'</pre>';
+
+
+		$requestParamsFieldeditor2 = array(
+		    "objectID" => $atts['object_id'],
+		    "section" => $atts['section'],
+		    "field" => $field_output
+		 
+		);
+
+		$response_field2 = $this->client->object()->retrieveFields($requestParamsFieldeditor2);
+
+		$response_field_decode2 = json_decode($response_field2);
+
+
+		$response_field_decode2_options = json_decode($response_field_decode2->data->options);
+
+		$val = "";
+
+		foreach($response_field_decode2_options as $response_field_decode2_option) {
+
+			$value = $response_field_decode2_option->value;
+			$label = $response_field_decode2_option->label;
+			$color = $response_field_decode2_option->color;
+			$bg_color = $response_field_decode2_option->backgroundColor;
+
+			$val .= "<h3><strong>Registration ".$atts['field_alias']."</strong></h3>";
+			$val .= "<p><strong>Value:</strong> ".$value."</p>";
+			$val .= "<p><strong>Label:</strong> ".$label."</p>";
+			$val .= "<p style='color: ".$color."'><strong >Color:</strong> ".$color."</p>";
+			$val .= "<p style='padding: 10px; background-color: ".$bg_color."'><strong>Background Color:</strong> ".$bg_color."</p>";
+			$val .= "<br/>";
+			//return'<pre>'.print_r($response_field_decode2_options , true).'</pre>';
+
+
+		}
+		
+		return $val;
+
+
+	}
+
+	public function wp_ontraninja_register_information_func($atts) {
+
+		$atts = shortcode_atts( array(
+	        'register_id' => ''
+	       
+	    ), $atts, 'wp_ontraninja_register_information' );
+
+
+		ob_start();
+
+	    ?>
+
+	    <div class="demo">
+		    <div class="container">
+		        <div class="row">
+
+		        	<?php
+
+		        	$requestParams = array(
+					    "objectID" => 10029, 
+					    "id"       => $atts['register_id']
+					    //"id"       => 37
+					);
+
+					$response = $this->client->object()->retrieveSingle($requestParams);
+
+					$response_decode = json_decode($response);
+
+					$object_vars = get_object_vars($response_decode->data);
+
+
+	        		$response_field = $this->client->object()->retrieveFields(array(
+					    "objectID" => 10029,
+					    "section" => "Registration Information"
+					));
+
+					$response_field_decode = json_decode($response_field);
+
+					$get_fieldeditor = array();
+
+					if(isset($response_field_decode->data->fields)) {
+
+						
+
+						$get_fieldeditor = $response_field_decode->data->fields;
+					
+					}
+
+
+
+		        	$reg_value = "";
+					$gf_alias = "";
+					$gf_field = "";
+					$bgc = "";
+
+		        	foreach($get_fieldeditor as $g_field => $pp) {
+
+		        		foreach($pp as $p) {
+
+
+
+				        	?>
+
+				            <div class="col-md-4 col-sm-6 mb-4">
+
+				                <div class="pricingTable <?php echo $p->field; ?>">
+				                    <div class="pricingTable-header">
+				                        <h3 class="title"><?php echo $p->alias; ?></h3>
+				                        <span class="duration"><?php echo $p->field; ?></span>
+				                    </div>
+
+				                    <?php
+
+				                    	$options_decode = json_decode($p->options);
+
+				                    	if(is_array($options_decode)) {
+
+					                    	if(!empty($options_decode)) {
+
+					                    		foreach($options_decode as $options_decod) {
+					                    			if(isset($options_decod->value)) {
+					                    				if($options_decod->value == $object_vars[$p->field]) {
+
+										                    ?>
+											                    <div class="pricing-content">
+											                        <div class="price-value <?php echo $p->field; ?>">
+											                           
+											                          <span class="amount" style="font-size:20px">Value - <?php echo $options_decod->value; ?></span>
+											                        </div>
+											                        <ul class="inner-content">
+											                            <li>Label - <?php echo $options_decod->label; ?></li>
+											                            <li>
+											                            	
+																		<?php
+
+																			if(isset($options_decod->color)) {
+
+																				?>
+																				 	<style>
+																				 		.pricingTable.<?php echo $p->field; ?> h3,
+																				 		.pricingTable.<?php echo $p->field; ?> span,
+																				 		.pricingTable.<?php echo $p->field; ?> p
+																				 		{
+																				 			color: <?php echo $options_decod->color; ?> !important;
+																						 }
+
+																						.pricingTable .price-value.<?php echo $p->field; ?> h3,
+																						.pricingTable .price-value.<?php echo $p->field; ?> span,
+																						.pricingTable .price-value.<?php echo $p->field; ?> p
+																				 		{
+																				 			color: <?php echo $options_decod->color; ?> !important;
+																						 }
+																				 	</style>
+																				 <?php
+
+																				echo '<pre>Color - '.print_r($options_decod->color, true).'</pre>';
+
+																			}
+
+																		?>
+
+											                            </li>
+
+											                            <li>
+
+											                            <?php 
+
+																			if(isset($options_decod->backgroundColor)) {
+
+																				$bgc .= $options_decod->backgroundColor.',';
+
+																				 ?>
+																				 	<style>
+																				 		.pricingTable.<?php echo $p->field; ?>
+																				 		{
+																				 			background: <?php echo $options_decod->backgroundColor; ?> !important;
+																						 }
+
+																						.pricingTable .price-value.<?php echo $p->field; ?>
+																				 		{
+																				 			background: <?php echo $options_decod->backgroundColor; ?> !important;
+																						 }
+
+																				 	</style>
+																				 <?php
+																				echo '<pre>BackgroundColor - '.print_r($options_decod->backgroundColor, true).'</pre>';
+																			}
+
+											                            ?>
+											                            	
+											                            </li>
+											                       
+											                        </ul>
+											                    
+											                    </div>
+
+										                    <?php
+
+								                		}
+								                	}
+							                	}
+					                    	}
+
+				                    	} else {
+				                    ?>
+				                    	<div class="pricing-content">
+					                        <div class="price-value">
+					                            <span class="amount" style="font-size:16px">Value - <?php 
+
+					                                $object_value =  $object_vars[$p->field]; 
+
+			                        if ((string) (int) $object_value === $object_value && ($object_value <= PHP_INT_MAX)
+        && ($object_value >= ~PHP_INT_MAX) && strlen($object_value) >= 10) {
+
+
+									$unix_timestamp = $object_value;
+
+
+								 	//$output_object_value = $object_value;
+
+									$datetime = new DateTime("@$unix_timestamp");
+								
+
+									$time_zone_to ='Australia/Sydney';
+									$format_time = 'j M Y';
+
+
+
+
+									$datetime = new DateTime("@$unix_timestamp");
+
+									$date_time_format = date_format($datetime, $format_time);
+
+
+
+									$time_zone_from = "UTC";
+
+
+									try {
+
+										$display_date = new DateTime($date_time_format, new DateTimeZone($time_zone_from));
+
+										$display_date->setTimezone(new DateTimeZone($time_zone_to));
+
+										$output_object_value = $display_date->format($format_time);
+
+									 } catch (Exception $e) {
+
+										$output_object_value = '<p><strong class="text-success">Date Data: </strong>Error Date and Timezone Format!</p>';
+
+									}
+
+
+
+			                        } else {
+
+			                        	  $output_object_value = $object_vars[$p->field];
+			                        }
+
+			                      	echo $output_object_value;
+
+			                           
+
+
+
+					                            ?></span>
+					                        </div>
+					                      
+					                    </div>
+
+
+				                    <?php
+				                    	}
+				                    		//echo '<pre>BackgroundColor - '.print_r($options_decod->backgroundColor, true).'</pre>';
+				                    ?>
+
+				                </div>
+				            </div>
+
+				            <?php
+
+		            	}
+
+		       		 }
+
+		            ?>
+
+
+		        </div>
+		    </div>
+
+		</div>
+
+	    <?php
+
+	    $output = ob_get_clean();
+
+	    return $output;
+	}
+
 		 
 }
 
